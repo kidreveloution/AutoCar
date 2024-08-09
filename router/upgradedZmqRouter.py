db4fdd9426d72b23220452b6acc65257a6393894
Alan <alyeldinshahin@gmail.com>
2024-08-09 01:32:52 -0400

    move

--- router/upgradedZmqRouter.py
+++ router/upgradedZmqRouter.py
@@ -17,10 +17,10 @@ while True:
     #worker_id = message[2].decode('utf-8')  # Decode as UTF-8 string
     ip_address = message[1].decode('utf-8')  # Decode the IP address as UTF-8
     message_content = message[2].decode('utf-8')  # Decode the message content as UTF-8
-    worker_id = message_content["address"]
-    print(f"Decoded worker_id: {worker_id}, IP: {ip_address}, Content: {message_content}")
 
     message_data = json.loads(message_content)
+    worker_id = message_content["address"]
+    print(f"Decoded worker_id: {worker_id}, IP: {ip_address}, Content: {message_content}")
 
     if message_data.get("msg_name") == "registeration":
         # Register the worker with its decoded ID and IP address
