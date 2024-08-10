48479d0ba9581a579f98be0c0531adb9fa101b37
Alan <alyeldinshahin@gmail.com>
2024-08-09 20:14:20 -0400

    Update upgradedZmqRouter.py

--- router/upgradedZmqRouter.py
+++ router/upgradedZmqRouter.py
@@ -17,15 +17,16 @@ while True:
     tx_id = message[1].decode('utf-8')
 
     message_content = message[2].decode('utf-8')  # Decode the message content as UTF-8
-
     message_data = json.loads(message_content)
+    
     rx_id = message_data["rx_id"]
-    ip_address = message_data["content"]["ip_address"]
 
-    print(f"Decoded tx_id: {tx_id}, IP: {ip_address}, Content: {message_content}")
 
     if message_data.get("msg_name") == "registeration":
         # Register the worker with its decoded ID and IP addresscd 
+        ip_address = message_data["content"]["ip_address"]
+
+        print(f"Decoded tx_id: {tx_id}, IP: {ip_address}, Content: {message_content}")
         connections[tx_id] = ip_address
         content = b"YOU HAVE BEEN REGISTERED"
         router.send_multipart([tx_id.encode('utf-8'), content])  # Encode the response back to UTF-8
