ec233d92c265e6ac0fbd011225327aa2e01e65cc
Alan <alyeldinshahin@gmail.com>
2024-08-17 16:45:49 -0400

    spelling

--- router/upgradedZmqRouter.py
+++ router/upgradedZmqRouter.py
@@ -23,7 +23,7 @@ while True:
         rx_id = message_data["rx_id"]
 
         print(message_data)
-        if message_data["msg_name"] == "registeration":
+        if message_data["msg_name"] == "register":
             # Register the worker with its decoded ID and IP addresscd 
             ip_address = message_data["content"]["ip_address"]
 
